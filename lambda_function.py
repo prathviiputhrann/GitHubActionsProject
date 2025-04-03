271db2c274f6a892f598317a7a84af9caf5b077d
prathviiputhrann <prathvii.puthrann@gmail.com>
2025-04-03 23:40:08 +0530

    #7 test workflow

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    message = "Hellooooo from the Lambda!"
+    message = "Helloooooo from the Lambda!"
     print(f"Lambda Output: {message}")  # Logs output in CloudWatch
     return {
         'statusCode': 200,
