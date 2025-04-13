39cc9f22b0348765af41405aad0e7de6fbe92c5c
prathviiputhrann <prathvii.puthrann@gmail.com>
2025-04-14 00:14:24 +0530

    Adding changes

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ provider "aws" {
 }
 
 # IAM ROLE: Creates an IAM role for Lambda Execution
-resource "aws_iam_role" "lambda_exec_role2" {
-  name = "lambda_exec_role2"
+resource "aws_iam_role" "lambda_exec_roles" {
+  name = "lambda_exec_roles"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -21,7 +21,7 @@ resource "aws_iam_role" "lambda_exec_role2" {
 
 # IAM POLICY: Attach AWS Managed Policies for Lambda Permissions
 resource "aws_iam_role_policy_attachment" "lambda_basic" {
-  role       = aws_iam_role.lambda_exec_role2.name
+  role       = aws_iam_role.lambda_exec_roles.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
@@ -37,7 +37,7 @@ resource "aws_lambda_function" "my_lambda" {
   function_name    = "MyLambdaFunction"
   filename         = data.archive_file.lambda_zip.output_path
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-  role             = aws_iam_role.lambda_exec_role2.arn
+  role             = aws_iam_role.lambda_exec_roles.arn
   handler         = "lambda_function.lambda_handler"
   runtime         = "python3.8"
 }
